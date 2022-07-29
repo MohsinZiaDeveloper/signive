e2c2495f750cbc67d3a39a05f428a6dc3ea1aa05
Mohsinzia01 <macbookpro@MacBooks-MacBook-Pro.local>
2022-07-30 02:55:40 +0500

    added place holder

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -65,8 +65,11 @@ const Footer = () => {
                 najszybciej jak to możliwe.
               </p>
               <button className="mt-5 w-[18rem] lg:w-auto text-xs font-light text-white pl-8 relative pr-20  bg-white rounded-full">
-            
-                <input  type="text" className="py-4 border-0 outline-0 text-black"/>
+                <input
+                  type="text"
+                  placeholder="wpisz adres email"
+                  className="py-4 border-0 outline-0 text-black"
+                />
                 <div className="h-10 w-10 absolute flex items-center justify-center bg-logoColor top-1 right-1 rounded-full">
                   <img
                     className="h-5 w-5 rounded-full"
